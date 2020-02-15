6b8e73137c63bbcbe1e5f70faa3d18e8a3f8b82e
Xuefeng Zhu <xzhu15@illinois.edu>
2020-02-15 16:33:46 -0700

    add initial web

---
+++ web/src/routes/IndexPage.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'dva';
+import styles from './IndexPage.css';
+import {
+  Button
+} from 'antd';
+function IndexPage() {
+  return (
+    <div className={styles.normal}>
+      <h1 className={styles.title}>Yay! Welcome to dva!</h1>
+      <div className={styles.welcome} />
+      <ul className={styles.list}>
+        <li>To get started, edit <code>src/index.js</code> and save to reload.</li>
+        <li><a href="https://github.com/dvajs/dva-docs/blob/master/v1/en-us/getting-started.md">Getting Started</a></li>
+      </ul>
+      <Button>Test</Button>
+    </div>
+  );
+}
+
+IndexPage.propTypes = {
+};
+
+export default connect()(IndexPage);
